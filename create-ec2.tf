5b66a8cd08a01ffdd66ee2252f1cdd54c9a5c9ef
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-10 09:40:10 +0530

    security group added

--- create-ec2.tf
+++ create-ec2.tf
@@ -2,6 +2,8 @@ resource "aws_instance" "web" {
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
 
+  vpc_security_group_ids = ["sg-0490a4409ed09ba21"]
+
   tags = {
     Name = "HelloWorld"
   }
