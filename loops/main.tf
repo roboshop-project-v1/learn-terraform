b62af9c48e50adcfafc5ee79e09cac7adf24ae14
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-10 11:37:45 +0530

    okokoko

--- loops/main.tf
+++ loops/main.tf
@@ -1,19 +1,43 @@
 
+# variable "components" {
+#     default = ["catalogue","frontend","mongodb"]
+  
+# }
+
+
+
+# resource "aws_instance" "web" {
+#   count = length(var.components)
+#   ami           = "ami-0f3c7d07486cad139"
+#   instance_type = "t2.micro"
+
+#   vpc_security_group_ids = ["sg-0490a4409ed09ba21"]
+
+#   tags = {
+#     Name = element(var.components,count.index)
+#   }
+# }
+
+
 variable "components" {
-    default = ["catalogue","frontend","mongodb"]
+    default = {
+
+    frontend = "frontend"
+    mongodb = "mongodb"
+    catalogue = "catalogue"
+
+    }
   
 }
 
-
-
 resource "aws_instance" "web" {
-  count = length(var.components)
+  for_each = var.components
   ami           = "ami-0f3c7d07486cad139"
   instance_type = "t2.micro"
 
   vpc_security_group_ids = ["sg-0490a4409ed09ba21"]
 
   tags = {
-    Name = element(var.components,count.index)
+    Name = lookup(var.components,each.key,null)
   }
 }
