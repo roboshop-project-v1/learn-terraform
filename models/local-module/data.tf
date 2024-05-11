090c6c48f949128ffa6fe790ed54c30b9e0b80b5
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-11 14:36:19 +0530

    okok

--- models/local-module/data.tf
+++ models/local-module/data.tf
@@ -7,3 +7,17 @@ output "name" {
     value = data.aws_ami_ids.ami
   
 }
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f3c7d07486cad139"
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = ["sg-0490a4409ed09ba21"]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+variable "instance_type" {}
--- models/main.tf
+++ models/main.tf
@@ -1,9 +1,14 @@
 module "test" {
   source = "./local-module"
+  instance_type = var.instance_type
 }
-
 
 output "ami_info" {
     value = module.test
+  
+}
+
+variable "instance_type" {
+    default = "t2.micro"
   
 }
