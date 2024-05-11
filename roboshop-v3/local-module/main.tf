f5dc18ad97c8f42957dd7739db22d379008f46ba
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-11 15:46:50 +0530

    okok

--- roboshop-v3/local-module/main.tf
+++ roboshop-v3/local-module/main.tf
@@ -15,3 +15,19 @@ resource "aws_route53_record" "record" {
   ttl     = 30
   records = [aws_instance.instance.private_ip]
 }
+
+resource "null_resource" "ansible" {
+  depends_on = [ aws_route53_record.record ]
+
+  provisioner "local-exec" {
+    command = <<EOF
+    cd /home/centos/roboshop-ansible
+    git pull
+    sleep 30
+    ansible-playbook -i ${var.component}.jdevops.online, -e ansible_user=centos -e ansible_password=Devops321 main.yml -e component=${var.component}
+
+    EOF
+  }
+  
+}
+
