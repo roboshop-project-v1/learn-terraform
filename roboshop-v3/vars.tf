c0686e8cd1ddd1c3d652b435d4dae4aae945aa0a
jagadeesh6jaga <jagadeesh6jaga@gmail.com>
2024-05-11 16:06:20 +0530

    okok

--- roboshop-v3/vars.tf
+++ roboshop-v3/vars.tf
@@ -21,6 +21,14 @@ variable "components" {
   default = {
       frontend  = {},
       mongodb   = {},
+      catalogue = {},
+      redis     = {},
+      user      = {},
+      cart      = {},
+      mysql     = {},
+      shipping  = {},
+      rabbitmq  = {},
+      payment   = {}
 
   }
 
